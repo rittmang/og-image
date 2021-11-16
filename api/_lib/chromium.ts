55778cee3854ee844a5cac862b37489d5ae23b57
Ritom Gupta <ritomgupta99@gmail.com>
2021-11-16 23:43:31 +0530

    dynamic wait time

--- api/_lib/chromium.ts
+++ api/_lib/chromium.ts
@@ -3,6 +3,37 @@ import { getOptions } from './options';
 import { FileType } from './types';
 let _page: core.Page | null;
 
+const waitTillHTMLRendered = async (page, timeout = 30000) => {
+    const checkDurationMsecs = 1000;
+    const maxChecks = timeout / checkDurationMsecs;
+    let lastHTMLSize = 0;
+    let checkCounts = 1;
+    let countStableSizeIterations = 0;
+    const minStableSizeIterations = 3;
+  
+    while(checkCounts++ <= maxChecks){
+      let html = await page.content();
+      let currentHTMLSize = html.length; 
+  
+      let bodyHTMLSize = await page.evaluate(() => document.body.innerHTML.length);
+  
+      console.log('last: ', lastHTMLSize, ' <> curr: ', currentHTMLSize, " body html size: ", bodyHTMLSize);
+  
+      if(lastHTMLSize != 0 && currentHTMLSize == lastHTMLSize) 
+        countStableSizeIterations++;
+      else 
+        countStableSizeIterations = 0; //reset the counter
+  
+      if(countStableSizeIterations >= minStableSizeIterations) {
+        console.log("Page rendered fully..");
+        break;
+      }
+  
+      lastHTMLSize = currentHTMLSize;
+      await page.waitFor(checkDurationMsecs);
+    }  
+  };
+
 async function getPage(isDev: boolean) {
     if (_page) {
         return _page;
@@ -16,9 +47,8 @@ async function getPage(isDev: boolean) {
 export async function getScreenshot(html: string, type: FileType, isDev: boolean) {
     const page = await getPage(isDev);
     await page.setViewport({ width: 1280, height: 720 });
-    await page.setContent(html,{
-        waitUntil:'networkidle2'
-    });
+    await page.setContent(html);
+    await waitTillHTMLRendered(page);
     const file = await page.screenshot({ type });
     return file;
 }
